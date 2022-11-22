2145355f9c4768ad3f87ccef276bf75dac824f93
MechatronicsCherepanovMA <118721438+MechatronicsCherepanovMA@users.noreply.github.com>
2022-11-22 12:22:12 +0300

    fix Ex1.3.2

--- Matlab/Ex1.3.2/Ex1.m
+++ Matlab/Ex1.3.2/Ex1.m
@@ -7,6 +7,8 @@ h = 100;
 x = (2*R).*cos(t)-h.*cos(2*t);
 y = (2*R).*sin(t)-h.*sin(2*t);
 plot(y,x)
+axis equal tight
+title('Улитка Паскаля');
 
 %Фигуры Лиссажу
 A = 100;
@@ -23,6 +25,8 @@ x = A.*sin(a*t+f);
 y = B.*sin(b*t);
 figure
 plot(x,y)
+axis equal tight
+title('Фигуры Лиссажу');
 
 %трохойда
 r = 40;
@@ -33,6 +37,8 @@ x = r*t - h*sin(t);
 y = r - h * cos(t);
 figure
 plot(x,y)
+axis equal tight
+title('Трохойда');
 
 %Гипоциклойда
 R = 6;
@@ -44,6 +50,8 @@ x = r*(k - 1)*(cos(t)+cos((k-1)*t)/(k-1));
 y = r*(k - 1)*(sin(t)- sin((k-1)*t)/(k-1));
 figure
 plot(x,y)
+axis equal tight
+title('Гипоциклойда');
 
 %Гипотрохиойда
 R = 6;
@@ -55,6 +63,8 @@ x = (R-r)*cos(f)+h*cos((R-r)/r*f);
 y = (R-r)*sin(f)+h*sin((R-r)/r*f);
 figure
 plot(x,y)
+axis equal tight
+title('Гипотрохиойда');
 
 %Эпициклойда
 R = 6;
@@ -66,9 +76,37 @@ x = (R+r)*cos(f)-r*cos(a+(R+r)/r*f);
 y = (R+r)*sin(f)-r*sin(a+(R+r)/r*f);
 figure
 plot(x,y)
+axis equal tight
+title('Эпициклойда');
 
-%Задача 3
-%Исходные данные
+%Гипотрохиойда
+R = 6;
+r = 1;
+f = 1:1000;
+f = f *pi/180;
+h= 10;
+x = (R-r)*cos(f)+h*cos((R-r)/r*f);
+y = (R-r)*sin(f)+h*sin((R-r)/r*f);
+figure
+plot(x,y)
+axis equal tight
+title('Гипотрохиойда');
+
+%Эпициклойда
+R = 6;
+r = 1;
+f = 1:360;
+f = f *pi/180;
+a = 10;
+x = (R+r)*cos(f)-r*cos(a+(R+r)/r*f);
+y = (R+r)*sin(f)-r*sin(a+(R+r)/r*f);
+figure
+plot(x,y)
+axis equal tight
+title('Эпициклойда');
+
+%Задача 3 
+%сходные данные
 k1 = 10;
 k2 = -2;
 k3 = -5;
@@ -77,16 +115,28 @@ T2 = 6;
 T3 = 8;
 
 %Уравнения
-dt1 = 1:T1;
+%Этап 1
+dt = 0:T1;
 V1 = k1*dt;
+%Этап 2
+dt = 1:T2;
+V2 = k1*T1 + k2*dt;
+%Этап 3
+dt = 1:T3;
+V3 = k1*T1 + k2*T2 + k3*dt;
+%Общий график
+dt = 0 : T1 + T2 + T3;
+v = [V1 V2 V3];
+figure
+plot(dt,v)
+axis equal tight
+title('График задачи')
 
-dt2 = T1 + 1:T2;
-V2 = k2*dt2;
 
-dt3 = T2 + 1:T3;
-V3 = k3*dt3;
 
-plot(V,dt)
+
+
+
 
 
 
