4f9d5b293a3f3e938299d744b19dbcde61505286
MechatronicsCherepanovMA <118721438+MechatronicsCherepanovMA@users.noreply.github.com>
2023-01-08 00:27:58 +0300

    08.01.23

--- Matlab/Ex1.3.4/ef1.m
+++ Matlab/Ex1.3.4/ef1.m
@@ -1,4 +1,3 @@
 function dxdt = ef1(t,x)
-%1 - x;2 - v; 3 - f;4 - w;5 - z,6 - a,7 - b,8 - c.
     x(1) = x(1) .* -1;
     dxdt = x;
--- Matlab/Ex1.3.4/ef3.m
+++ Matlab/Ex1.3.4/ef3.m
@@ -1,6 +1,5 @@
-function dxdt = ef3(t,x)
-    %1 - x;2 - v; 3 - f;4 - w;5 - z,6 - a,7 - b,8 - c.
-    rx = x;
-    rx(2) = -x(6) * x(1) - x(7) + x(8) * sin(x(4) * t + x(3));
-    rx(1) = x(2);
-    dxdt = rx;
+function dxdt = ef3(t,x,a,b,w,f,c)
+    fx = x;
+    fx(2) = -a * x(1) - b * x(2) + c * sin(w * t + f);
+    fx(1) = x(2);
+    dxdt = fx;
